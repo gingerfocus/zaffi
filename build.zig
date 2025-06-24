ff3eab710e068fe6932d70f0c97047a5aa0de8c9
evan stokdyk <evan.stokdyk@gmail.com>
2025-06-24 16:25:41 -0700

    make module public

--- build.zig
+++ build.zig
@@ -4,7 +4,7 @@ pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
 
-    const zaffi = b.createModule(.{
+    const zaffi = b.addModule("zaffi", .{
         .root_source_file = b.path("src/lib.zig"),
         .target = target,
         .optimize = optimize,
